f3493a92af81f83c9c8a7f42ccc6fac422b53094
Gowsikakho <arulgowsi3357@gmail.com>
2026-01-23 11:57:32 +0530

    practices conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,19 @@
-import React from "react"
-import avatar from "./assets/images/user.png"
-import starFilled from "./assets/images/star-filled.png"
-import starEmpty from "./assets/images/star-empty.png"
+import Joke from "./jokes.jsx"
+import jokesData from "./JokeData.jsx"
 
 export default function App() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (212) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: true
+    const jokeElements = jokesData.map(joke => {
+        return (
+            <Joke 
+                key={joke.id}
+                setup={joke.setup} 
+                punchline={joke.punchline} 
+            />
+        )
     })
-    
-    let starIcon = contact.isFavorite ? starFilled : starEmpty
-
-    function toggleFavorite() {
-        setContact(prevContact => {
-            return {
-                ...prevContact,
-                isFavorite: !prevContact.isFavorite
-            }
-        })
-    }
-    
-    /**
-     * Challenge:
-     * Update the following:
-     * - aria-pressed should reflect the same value as contact.isFavorite.
-     * - aria-label should switch to say "Remove from favorites" if
-     *   contact.isFavorite is `true`.
-     * - img alt should say "filled star icon" when it is filled.
-     */
-
     return (
-        <main>
-            <article className="card">
-                <img
-                    src={avatar}
-                    className="avatar"
-                    alt="User profile picture of John Doe"
-                />
-                <div className="info">
-                    <button
-                        onClick={toggleFavorite}
-                        aria-pressed={false}
-                        aria-label="Add to favorites"
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt="empty star icon"
-                            className="favorite"
-                        />
-                    </button>
-                    <h2 className="name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
-
-            </article>
-        </main>
+        <div>
+            {jokeElements}
+        </div>
     )
 }
--- src/jokes.jsx
+++ src/jokes.jsx
@@ -1,8 +1,21 @@
+import React from "react"
+
 export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+
+    function toggle() {
+        setIsShown(prev => !prev)
+    }
+
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+
+            {isShown && <p>{props.Punchline}</p>}
+
+            <button onClick={toggle}>
+                {isShown ? "Hide Punchline" : "Show Punchline"}
+            </button>
         </div>
     )
 }
